606964b478298279bbda2a621593447cec9c2f9c
Mortada50 <mortadaabdullahaltaj@gmail.com>
2025-12-02 21:34:28 +0300

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,37 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./config/env.js";
-
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { connectDB } from "./config/db.js";
+import { functions, inngest } from "./config/inngest.js";
 const app = express();
 
 const __dirname = path.resolve();
 
+app.use(clerkMiddleware()); // adds auth object under the req => req.auth
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.get("/api/health", (req, res) => {
-    res.status(200).json({message: "Success"})
-})
+  res.status(200).json({ message: "Success" });
+});
 
 // make our app ready for deployment
 
-if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../admin/dist")))
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"))
-    })
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
 }
 
-app.listen(ENV.PORT, () => console.log("Server is up and running"));
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("Server is up and running");
+  });
+};
 
+startServer();
