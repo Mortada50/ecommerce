4fdcb701e221b45d17bf66a440a740a6893e598b
Mortada50 <mortadaabdullahaltaj@gmail.com>
2025-12-07 20:38:30 +0300

    cart routes & controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import path from "path";
 import { ENV } from "./config/env.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import { functions, inngest } from "./config/inngest.js";
 import adminRoutes from "./routes/admin.route.js"
@@ -10,13 +11,15 @@ import userRoutes from "./routes/user.route.js";
 import orderRoutes from "./routes/order.route.js";
 import reviewRoutes from "./routes/review.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 app.use(express.json());
 app.use(clerkMiddleware()); // adds auth object under the req => req.auth
-
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the
+// browser to send the cookies to the server with the request
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
@@ -25,6 +28,7 @@ app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
 app.use("/api/reviews", reviewRoutes)
 app.use("/api/products", productRoutes)
+app.use("/api/cart", cartRoutes)
 
 
 app.get("/api/health", (req, res) => {
