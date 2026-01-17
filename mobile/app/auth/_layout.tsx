96b0b97b9b1a07c25772e6734997e73075942f52
Mortada50 <mortadaabdullahaltaj@gmail.com>
2026-01-17 23:58:48 +0300

    auth implemented in mobile folder

--- mobile/app/auth/_layout.tsx
+++ mobile/app/auth/_layout.tsx
@@ -3,7 +3,8 @@ import { useAuth } from '@clerk/clerk-expo';
 
 export default function AuthRoutesLayout() {
   const { isSignedIn } = useAuth();
-
+  
+  
   if (isSignedIn) {
      return <Redirect href={"/"} />;
   }
