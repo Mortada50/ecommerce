d52e2b02ae329698dec366725dc0b760f5e6e583
Mortada50 <mortadaabdullahaltaj@gmail.com>
2026-02-04 18:17:37 +0300

    wishlist screen added

---
+++ mobile/app/(profile)/addresses.tsx
@@ -0,0 +1,11 @@
+
+import SafeScreen from '@/components/SafeScreen'
+import { View } from 'react-native'
+
+function AddressesScreen() {
+  return (
+    <SafeScreen>OrdersScreen</SafeScreen>
+  )
+}
+
+export default AddressesScreen
---
+++ mobile/app/(profile)/privacy-security.tsx
@@ -0,0 +1,11 @@
+
+import SafeScreen from '@/components/SafeScreen';
+import { View } from 'react-native'
+
+function PrivacyAndSecurityScreen() {
+  return (
+    <SafeScreen>OrdersScreen</SafeScreen>
+  )
+}
+
+export default PrivacyAndSecurityScreen;
